77c9224a64a76477676ea4eeba8c8571b45c9019
Ryan Yosua <rcy5017@psu.edu>
2014-09-06 16:34:45 -0400

    Debug logs.

--- src/edu/psu/rcy5017/publicspeakingassistant/MainActivity.java
+++ src/edu/psu/rcy5017/publicspeakingassistant/MainActivity.java
@@ -3,16 +3,20 @@ package edu.psu.rcy5017.publicspeakingassistant;
 import edu.psu.rcy501.publicspeakingassistant.R;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
 public class MainActivity extends ActionBarActivity {
+	
+	private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(TAG, "Main acticity started");
     }
 
 
--- src/edu/psu/rcy5017/publicspeakingassistant/SplashScreenActivity.java
+++ src/edu/psu/rcy5017/publicspeakingassistant/SplashScreenActivity.java
@@ -5,15 +5,20 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 
 public class SplashScreenActivity extends Activity {
-	// Splash screen timer
-    private static int SPLASH_TIME_OUT = 3000;
- 
+	
+	private static final String TAG = "SplashScreenActivity";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        
+        Log.d(TAG, "Splash acticity started");
+        
+        final int SPLASH_TIME_OUT = 3000;
  
         new Handler().postDelayed(new Runnable() {
  
@@ -25,15 +30,10 @@ public class SplashScreenActivity extends Activity {
             @Override
             public void run() {
                 // This method will be executed once the timer is over
-                // Start your app main activity
+                // Start app main activity
                 Intent i = new Intent(SplashScreenActivity.this, MainActivity.class);
-                try {
-					startActivity(i);
-				} catch (Exception e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
- 
+                startActivity(i);
+		
                 // close this activity
                 finish();
             }
