445241be9f262a296f07168c422efc24f09466fa
Ryan Yosua <rcy5017@psu.edu>
2014-09-24 23:41:39 -0400

    Added abstract class DataSource that all datasources will be sub classes of.

---
+++ src/edu/psu/rcy5017/publicspeakingassistant/datasource/DataSource.java
@@ -0,0 +1,32 @@
+package edu.psu.rcy5017.publicspeakingassistant.datasource;
+
+import android.content.Context;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import edu.psu.rcy5017.publicspeakingassistant.DatabaseHelper;
+
+public abstract class DataSource {
+	
+	private SQLiteDatabase database;
+	private DatabaseHelper dbHelper;
+	
+	public DataSource(Context context) {
+		 dbHelper = new DatabaseHelper(context);
+	}
+	
+    public void open() throws SQLException {
+        database = getDbHelper().getWritableDatabase();
+    }
+
+    public void close() {
+        getDbHelper().close();
+    }
+	
+	public SQLiteDatabase getDatabase() {
+		return database;
+	}
+
+	public DatabaseHelper getDbHelper() {
+		return dbHelper;
+	}
+}
--- src/edu/psu/rcy5017/publicspeakingassistant/datasource/SpeechDataSource.java
+++ src/edu/psu/rcy5017/publicspeakingassistant/datasource/SpeechDataSource.java
@@ -6,37 +6,24 @@ import java.util.List;
 import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
-import android.database.SQLException;
-import android.database.sqlite.SQLiteDatabase;
 import edu.psu.rcy5017.publicspeakingassistant.DatabaseHelper;
 import edu.psu.rcy5017.publicspeakingassistant.model.Speech;
 
-public class SpeechDataSource {
+public class SpeechDataSource extends DataSource {
 
-    // Database fields
-    private SQLiteDatabase database;
-    private DatabaseHelper dbHelper;
     private String[] allColumns = { DatabaseHelper.COLUMN_ID,
             DatabaseHelper.SPEECH_TITLE };
 
     public SpeechDataSource(Context context) {
-        dbHelper = new DatabaseHelper(context);
-    }
-
-    public void open() throws SQLException {
-        database = dbHelper.getWritableDatabase();
-    }
-
-    public void close() {
-        dbHelper.close();
+        super(context);
     }
 
     public Speech createSpeech(String title) {
         ContentValues values = new ContentValues();
         values.put(DatabaseHelper.SPEECH_TITLE, title);
-        long insertId = database.insert(DatabaseHelper.SPEECH_TABLE_NAME, null,
+        long insertId = getDatabase().insert(DatabaseHelper.SPEECH_TABLE_NAME, null,
                 values);
-        Cursor cursor = database.query(DatabaseHelper.SPEECH_TABLE_NAME,
+        Cursor cursor = getDatabase().query(DatabaseHelper.SPEECH_TABLE_NAME,
                 allColumns, DatabaseHelper.COLUMN_ID + " = " + insertId, null,
                 null, null, null);
         cursor.moveToFirst();
@@ -48,7 +35,7 @@ public class SpeechDataSource {
     public void deleteSpeech(Speech speech) {
         long id = speech.getId();
         System.out.println("Speech deleted with id: " + id);
-        database.delete(DatabaseHelper.SPEECH_TABLE_NAME, DatabaseHelper.COLUMN_ID
+        getDatabase().delete(DatabaseHelper.SPEECH_TABLE_NAME, DatabaseHelper.COLUMN_ID
                 + " = " + id, null);
     }
     
@@ -61,14 +48,14 @@ public class SpeechDataSource {
     public int renameSpeech(Speech speech, String newTitle) {
     	final ContentValues args = new ContentValues();
         args.put(DatabaseHelper.SPEECH_TITLE, newTitle);
-        return database.update(
+        return getDatabase().update(
         		DatabaseHelper.SPEECH_TABLE_NAME, args, DatabaseHelper.COLUMN_ID + "=" + speech.getId(), null);
     }
 
     public List<Speech> getAllSpeeches() {
         List<Speech> speeches = new ArrayList<Speech>();
 
-        Cursor cursor = database.query(DatabaseHelper.SPEECH_TABLE_NAME,
+        Cursor cursor = getDatabase().query(DatabaseHelper.SPEECH_TABLE_NAME,
                 allColumns, null, null, null, null, null);
 
         cursor.moveToFirst();
