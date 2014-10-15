466244435f984149102cb5a79bcbb5efe2469650
Ryan Yosua <rcy5017@psu.edu>
2014-10-14 22:31:25 -0400

    Complete model class.

--- src/edu/psu/rcy5017/publicspeakingassistant/model/SpeechRecording.java
+++ src/edu/psu/rcy5017/publicspeakingassistant/model/SpeechRecording.java
@@ -1,5 +1,43 @@
 package edu.psu.rcy5017.publicspeakingassistant.model;
 
 public class SpeechRecording {
+    
+    private final long id;
+    private final long speechID;
+    
+    private String title;
+    private String file;
+    
+    public SpeechRecording(long id, String title, long speechID) {
+        this.id = id;
+        this.title = title;
+        this.file = this.id + "";
+        this.speechID = speechID;
+    }
+     
+    public long getId() {
+        return id;
+    }
+  
+    public String getTitle() {
+        return title;
+    }
 
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    
+    public String getFile() {
+        return file;
+    }
+    
+    public long getSpeechID() {
+        return speechID;
+    }
+    
+    // Will be used by the ArrayAdapter in the ListView
+    @Override
+    public String toString() {
+        return title;
+    }
 }
