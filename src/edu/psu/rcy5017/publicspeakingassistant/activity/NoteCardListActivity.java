35b0257417285ea6f92fc12ebcd90f25a03b53fa
Ryan Yosua <rcy5017@psu.edu>
2014-10-16 21:30:36 -0400

    Complete database operations in a background thread.

--- src/edu/psu/rcy5017/publicspeakingassistant/activity/NoteCardListActivity.java
+++ src/edu/psu/rcy5017/publicspeakingassistant/activity/NoteCardListActivity.java
@@ -1,14 +1,17 @@
 package edu.psu.rcy5017.publicspeakingassistant.activity;
 
 import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 import edu.psu.rcy5017.publicspeakingassistant.R;
 import edu.psu.rcy5017.publicspeakingassistant.constant.DefaultValues;
 import edu.psu.rcy5017.publicspeakingassistant.constant.RequestCodes;
 import edu.psu.rcy5017.publicspeakingassistant.datasource.NoteCardDataSource;
 import edu.psu.rcy5017.publicspeakingassistant.model.NoteCard;
+import edu.psu.rcy5017.publicspeakingassistant.task.NoteCardTask;
 import android.app.ListActivity;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.ContextMenu;
@@ -24,28 +27,36 @@ public class NoteCardListActivity extends ListActivity {
     
     private static final String TAG = "NoteCardListActivity";
     
+    private ArrayAdapter<NoteCard> adapter;
     private NoteCardDataSource datasource;
     private long speechID;
-
+   
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notecard_list);
         
         datasource = new NoteCardDataSource(this);
-        datasource.open();
         
         // Get the speechID passed from list activity.
         final Intent intent = this.getIntent();
         speechID = intent.getLongExtra("id", DefaultValues.DEFAULT_LONG_VALUE);
         
-        final List<NoteCard> values = datasource.getAllNoteCards(speechID);
+        List<NoteCard> values = null;
+        try {
+            values = new GetNoteCardsTask().execute().get();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
         
-        // Use the SimpleCursorAdapter to show the elements in a ListView.
-        final ArrayAdapter<NoteCard> adapter = 
-                new ArrayAdapter<NoteCard>(this, android.R.layout.simple_list_item_1, values);
-        setListAdapter(adapter);
-       
+        if (values != null) {
+            // Use the SimpleCursorAdapter to show the elements in a ListView.
+            adapter = new ArrayAdapter<NoteCard>(this, android.R.layout.simple_list_item_1, values);
+            setListAdapter(adapter);
+        }
+        
         // Register the ListView  for Context menu  
         registerForContextMenu(getListView());
     }
@@ -55,9 +66,6 @@ public class NoteCardListActivity extends ListActivity {
      * @param view the calling view
      */
     public void onClick(View view) {
-        @SuppressWarnings("unchecked")
-        final ArrayAdapter<NoteCard> adapter = (ArrayAdapter<NoteCard>) getListAdapter();
-        
         // Get the speechID passed from list activity.
         final Intent intent = this.getIntent();
        
@@ -66,15 +74,17 @@ public class NoteCardListActivity extends ListActivity {
         case R.id.add_note_card:
             
             final long speechID = intent.getLongExtra("id", DefaultValues.DEFAULT_LONG_VALUE);
-            final NoteCard noteCard = datasource.createNoteCard("New Note Card", speechID);
-            // Save the new notecard to the database.
-            adapter.add(noteCard);
-            // Force user to overwrite the default name.
-            renameNoteCard(noteCard, adapter.getCount() - 1);
             
+            try {
+                final NoteCard noteCard = new CreateNoteCardTask().execute().get();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            }
+         
             break;    
         }
-        adapter.notifyDataSetChanged();
     }
     
     @Override
@@ -95,8 +105,6 @@ public class NoteCardListActivity extends ListActivity {
         final AdapterContextMenuInfo info = (AdapterContextMenuInfo) item
                 .getMenuInfo();
         
-        @SuppressWarnings("unchecked")
-        final ArrayAdapter<NoteCard> adapter = (ArrayAdapter<NoteCard>) getListAdapter();
         final NoteCard noteCard = (NoteCard) getListAdapter().getItem(info.position);
         
         switch (item.getItemId()) {
@@ -110,9 +118,7 @@ public class NoteCardListActivity extends ListActivity {
                 return true;
         
             case R.id.delete_notecard:
-                datasource.deleteNoteCard(noteCard);
-                adapter.remove(noteCard);
-                adapter.notifyDataSetChanged();
+                new DeleteNoteCardTask(noteCard).execute();
                 return true;
         }
      
@@ -126,10 +132,7 @@ public class NoteCardListActivity extends ListActivity {
             final String newNoteCardTitle = data.getStringExtra("text");
             final NoteCard noteCard = new NoteCard(newNoteCardId, newNoteCardTitle, speechID);
             final int position = data.getIntExtra("position", DefaultValues.DEFAULT_INT_VALUE);
-            
-            @SuppressWarnings("unchecked")
-            final ArrayAdapter<NoteCard> adapter = (ArrayAdapter<NoteCard>) getListAdapter();
-            
+               
             // Get the note card item to update.
             final NoteCard noteCardToUpdate = 
                     adapter.getItem(position);
@@ -139,8 +142,7 @@ public class NoteCardListActivity extends ListActivity {
             adapter.notifyDataSetChanged();
             
             // Save the changes to the database.
-            datasource.open();
-            datasource.renameNotecard(noteCard, noteCard.getTitle());
+            new RenameNoteCardTask(noteCardToUpdate).execute();
         }
     }
 
@@ -156,5 +158,78 @@ public class NoteCardListActivity extends ListActivity {
         intent.putExtra("id", noteCard.getId());
         intent.putExtra("text", noteCard.getTitle());
         startActivityForResult(intent, RequestCodes.RENAME_NOTECARD_REQUEST_CODE);
+    }
+    
+    private class CreateNoteCardTask extends AsyncTask<Void, Void, NoteCard> {
+        
+        private NoteCard noteCard;
+
+        @Override
+        protected NoteCard doInBackground(Void... params) {
+            datasource.open();
+            noteCard = datasource.createNoteCard("New Note Card", speechID);
+            datasource.close();
+            return noteCard;
+        }
+        
+        @Override
+        protected void onPostExecute(NoteCard noteCard) {
+            // Save the new speech to the database.
+            adapter.add(noteCard);
+            // Force user to overwrite the default name.
+            renameNoteCard(noteCard, adapter.getCount() - 1);
+            adapter.notifyDataSetChanged();
+        }
+        
+    }
+    
+    private class DeleteNoteCardTask extends NoteCardTask
+    {  
+        public DeleteNoteCardTask(NoteCard noteCard) {
+            super(noteCard);
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            datasource.open();
+            datasource.deleteNoteCard(getNoteCard());
+            datasource.close();
+            return null;
+        }
+        
+        @Override
+        protected void onPostExecute(Void result) {
+            adapter.remove(getNoteCard());
+            adapter.notifyDataSetChanged();
+        }
+    }
+    
+    private class RenameNoteCardTask extends NoteCardTask {
+        
+        public RenameNoteCardTask(NoteCard noteCard) {
+            super(noteCard);
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            final NoteCard noteCard = getNoteCard();
+            datasource.open();
+            datasource.renameNotecard(noteCard, noteCard.getTitle());
+            datasource.close();
+            return null;
+        }
+    }
+    
+    private class GetNoteCardsTask extends AsyncTask<Void, Void, List<NoteCard>> {
+        
+        @Override
+        protected List<NoteCard> doInBackground(Void... params) {
+            datasource.open();
+            final List<NoteCard> values = datasource.getAllNoteCards(speechID);
+            datasource.close();
+            
+            return values;
+        }
+        
     }
 }
