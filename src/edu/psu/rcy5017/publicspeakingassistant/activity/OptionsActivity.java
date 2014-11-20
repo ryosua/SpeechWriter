1310489fe05b12d114f54ef13689bf74f3a7a807
Ryan Yosua <rcy5017@psu.edu>
2014-11-20 17:25:28 -0500

    Added blank options menu.

---
+++ src/edu/psu/rcy5017/publicspeakingassistant/activity/OptionsActivity.java
@@ -0,0 +1,15 @@
+package edu.psu.rcy5017.publicspeakingassistant.activity;
+
+import edu.psu.rcy5017.publicspeakingassistant.R;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class OptionsActivity extends Activity {
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_options);
+    }
+
+}
