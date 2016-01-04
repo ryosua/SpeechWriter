e74e44e6f6706436efd468181783e53f83f0be0b
Ryan Yosua <hi@ryanyosua.me>
2016-01-04 15:01:45 -0500

    Mixpanel event - "App installed"

--- app/src/main/java/edu/psu/rcy5017/speechwriter/activity/SplashScreenActivity.java
+++ app/src/main/java/edu/psu/rcy5017/speechwriter/activity/SplashScreenActivity.java
@@ -2,6 +2,7 @@ package edu.psu.rcy5017.speechwriter.activity;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
@@ -29,8 +30,23 @@ public class SplashScreenActivity extends Activity {
         
         Log.d(TAG, "Splash activity started");
 
-        // Record activity in mixpanel.
+        // Record an event if this was the first use of the app.
+        final String PREFS_NAME = "speech_writer_settings";
+
         mixpanel = MixpanelAPI.getInstance(this.getApplicationContext(), MixPanelCodes.MIXPANEL_TOKEN);
+
+        // Record that the app has been installed for the first time.
+        SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+        if (settings.getBoolean("first_time", true)) {
+            Log.d(TAG, "App is being used for the first time.");
+
+            mixpanel.track("App Installed");
+
+            settings.edit().putBoolean("first_time", false).commit();
+        } else {
+            Log.d(TAG, "The app has been used before.");
+        }
+
         mixpanel.track("Splash Screen Started");
 
         final int SPLASH_TIME_OUT = 1500;
