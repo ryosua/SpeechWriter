7b49100523e4e1ef9aa2293ee2e7209a69bd67d3
Ryan Yosua <hi@ryanyosua.me>
2016-03-25 17:20:17 -0400

    Added new empty activity.

---
+++ app/src/main/java/edu/psu/rcy5017/speechwriter/activity/SpeecherAdActivity.java
@@ -0,0 +1,15 @@
+package edu.psu.rcy5017.speechwriter.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import edu.psu.rcy5017.speechwriter.R;
+
+public class SpeecherAdActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_speecher_ad);
+    }
+}
